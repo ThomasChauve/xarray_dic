8dfcff202b5499c50ca8800eb0bf4094b28612a6
Thomas Chauve <thomaschauve@disroot.org>
2021-06-14 13:09:22 +0200

    first push

---
+++ xarray_dic/loadDIC.py
@@ -0,0 +1,135 @@
+import xarray as xr
+import numpy as np
+import os
+
+def loadDICe(adr_data, resolution, time_step,unit_time='min',unit_res='millimeter',strain_com=1):
+    '''
+    :param adr_data: folder path where the 7D output are store
+    :type adr_data: str
+    :param resolution: pixel size of the image used for DIC (millimeters)
+    :type resolution: float
+    :param time_step: time step between the picture (seconds)
+    :type time_step: float
+    :param adr_micro: path for the black and white skeleton of the microstructure (bmp format) (default 0 - no microstructure)
+    :type adr_micro: str
+    :param strain_com: Computation method for strain calculation 0-small deformation 1-Green Lagrange (default 1)
+    :type strain_com: bool
+    '''
+
+    # include time_step in the object
+    time=time_step
+
+    # find out file from 7D
+    output=os.listdir(adr_data+'results/')
+    output.sort()
+    output=output[2:-1]
+    # loop on all the output
+    all_sT=[]
+    all_da=[]
+    for i in list(range(len(output))):
+    #for i in [0]:
+        # load the file
+        data=np.loadtxt(adr_data+'results/'+ output[i],skiprows=1,delimiter=',',usecols=[1,2,3,4,9])
+        # find not indexed point
+        id=np.where(data[:,-1]==0)
+        # replace not indexed point by NaN value
+        data[id[0],2]=np.NaN
+        data[id[0],3]=np.NaN
+        # for the first step it extract size sx,sy and window correlation value used in 7D
+        if (i==0):
+            n_dic=np.abs(data[0,1]-data[1,1])
+            nb_pix=np.size(data[:,0])
+            sx=np.int32(1+np.abs(np.min(data[:,0])-np.max(data[:,0]))/n_dic)
+            sy=np.int32(1+np.abs(np.min(data[:,1])-np.max(data[:,1]))/n_dic)
+            dx=np.linspace(0,n_dic*resolution*(sx-1),sx)
+            dy=np.linspace(0,n_dic*resolution*(sy-1),sy)
+        
+        # Build image at time t=i
+        tmp_u=np.zeros([sx,sy])
+        tmp_u[:,:]=np.NaN
+        tmp_v=np.zeros([sx,sy])
+        tmp_v[:,:]=np.NaN
+        # find x y position in the table
+        x=(data[:,0]-np.min(data[:,0]))/n_dic
+        y=(data[:,1]-np.min(data[:,1]))/n_dic
+        # include data
+        tmp_u[x.astype(int),y.astype(int)]=data[:,2]
+        tmp_v[x.astype(int),y.astype(int)]=data[:,3]
+        # Create im2d data object u and v
+        imu=np.transpose(tmp_u*resolution)
+        imv=np.transpose(tmp_v*resolution)
+                
+        da=xr.DataArray(np.dstack((imu,imv)),dims=['y','x','vc'])
+        da.coords['x']=dx
+        da.coords['y']=dy
+        all_da.append(np.dstack((imu,imv)))
+        
+        diff_ux=da[:,:,0].diff('x')/(resolution*n_dic)
+        diff_ux=diff_ux.where(diff_ux.y>0,drop=True)
+        diff_vx=da[:,:,1].diff('x')/(resolution*n_dic)
+        diff_vx=diff_vx.where(diff_vx.y>0,drop=True)
+        
+        diff_uy=da[:,:,0].diff('y')/(resolution*n_dic)
+        diff_uy=diff_uy.where(diff_uy.x>0,drop=True)
+        diff_vy=da[:,:,1].diff('y')/(resolution*n_dic)
+        diff_vy=diff_vy.where(diff_vy.x>0,drop=True)
+        
+        empty=np.zeros(diff_ux.shape[0:2])
+        empty[:,:]=np.nan
+        # exx, eyy, ezz, exy, exz, eyz
+        if strain_com==0:
+            sT=np.dstack((
+                np.array(diff_ux),
+                np.array(diff_vy),
+                empty,
+                np.array((diff_uy+diff_vx)*0.5),
+                empty,
+                empty,
+            ))
+        else:
+            sT=np.dstack((
+                np.array(diff_ux+(diff_ux**2+diff_vx**2)*0.5),
+                np.array(diff_vy+(diff_uy**2+diff_vy**2)*0.5),
+                empty,
+                np.array((diff_uy+diff_vx)*0.5+(diff_ux*diff_uy+diff_vx*diff_vy)*0.5),
+                empty,
+                empty,
+            ))
+        
+#         sT[np.isnan(sT[:,:,1]),0]=np.nan
+#         sT[np.isnan(sT[:,:,1]),3]=np.nan
+
+#         sT[np.isnan(sT[:,:,0]),1]=np.nan
+#         sT[np.isnan(sT[:,:,0]),3]=np.nan
+
+#         sT[np.isnan(sT[:,:,3]),1]=np.nan
+#         sT[np.isnan(sT[:,:,3]),0]=np.nan
+        
+        all_sT.append(sT)
+       
+    dsT=xr.DataArray(np.stack(all_sT),dims=['time','yt','xt','sT'])
+    da=xr.DataArray(np.stack(all_da),dims=['time','yd','xd','d'])
+    
+    time=np.linspace(time_step,time_step*len(output),len(output))
+    
+    ds = xr.Dataset(
+    {   
+        "displacement": da,
+        "strain": dsT,
+
+    },
+    )
+    ds.coords['xd']=dx
+    ds.coords['yd']=dy
+    ds.coords['xt']=dx[1::]
+    ds.coords['yt']=dy[1::]
+    ds.coords['time']=time
+
+    ds.attrs["unit_time"]=unit_time
+    ds.attrs["step_size"]=resolution
+    ds.attrs["unit_position"]=unit_res
+    ds.attrs["window_size"]=int(n_dic)
+    ds.attrs["path_dat"]=adr_data
+            
+            
+    return ds#,sT,oxy
---
+++ xarray_dic/xarray_dic.py
@@ -0,0 +1,43 @@
+import xarray as xr
+import numpy as np
+
+@xr.register_dataset_accessor("dic")
+
+class xarray_dic(object):
+    '''
+    This is a classe to work on aita data in xarray environnement.
+    
+    .. note:: xarray does not support heritage from xr.DataArray may be the day it support it, we could move to it
+    '''
+    
+    def __init__(self, xarray_obj):
+        '''
+        Constructor for aita. 
+        
+        The xarray_obj should contained at least :
+        1. displacement : DataArray that contained displacement in the last column of dimention
+        2. strain : DataArray taht contained strain field compatible with xarray_symTensor2d
+        
+        :param xarray_obj:
+        :type xarray_obj: xr.Dataset
+        '''
+        self._obj = xarray_obj 
+    pass
+#--------------------geometric transformation---------------------------
+
+    def DIC_line(self,axis='x',shift=3):
+        '''
+        Compute the average of one componant
+        
+        :param axis: txx,tyy,tzz,txy,txz,tyz
+        :type axis: str
+        :return mean: average of this component
+        :rtype mean: xr.DataArray
+        '''
+        
+        if axis=='x':
+            sd=(np.nanmean(self._obj.displacement[...,:,-shift,0],axis=-1)-np.nanmean(self._obj.displacement[...,:,shift,0],axis=-1))/np.array((self._obj.xd[-shift]-self._obj.xd[shift]))
+        if axis=='y':
+            sd=(np.nanmean(self._obj.displacement[...,-shift,:,1],axis=-1)-np.nanmean(self._obj.displacement[...,shift,:,1],axis=-1))/np.array((self._obj.yd[-shift]-self._obj.yd[shift]))
+            
+        return xr.DataArray(sd,dims=self._obj.displacement.coords.dims[0])          
