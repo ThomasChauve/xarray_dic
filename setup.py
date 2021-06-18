6523f2323354c25a9faab5f056cac29878a8ed4e
Thomas Chauve <thomaschauve@disroot.org>
2021-06-18 09:42:30 +0200

    add dependency

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-#    'xarray','numpy','matplotlib','scikit-image'
+    'xarray','numpy','matplotlib','scikit-image'
 ]
 
 # What packages are optional?
